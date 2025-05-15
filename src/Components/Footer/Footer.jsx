741063faa490de3ca55e48d17502fd5e0e9ffba2
SanjarbekH02 <sanjarbekkhakimov21409@gmail.com>
2025-05-15 15:27:58 +0500

    diroktor ismi ish vaqti qoshildi

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
     return (
         <div id='contacts' className="contact-container">
-            <h3 className="services-title-top">{t('footer.contacts')}</h3>
+            <h1 className="services-title-top">{t('footer.contacts')}</h1>
             <h1 className="contact-heading">{t('footer.getInTouch')}</h1>
             <div className="footer">
                 <div className="contact-info">
@@ -15,6 +15,9 @@ const Footer = () => {
                     <p><i className="ri-phone-line"></i> <a href="tel:+998955518888">+998 95 551 8888</a></p>
                     <p><i className="ri-phone-line"></i> <a href="tel:+998981110045">+998 98 111 00 45</a></p>
                     <p><i className="ri-mail-line"></i> <a href="mailto:sofamarva.chorsu@gmail.com">sofamarva.chorsu@gmail.com</a></p>
+                    <p><i className="ri-user-line"></i> {t('footer.director')} Fathullayev Dilmurod Farhod O'g'li</p>
+                    <p><i className="ri-time-line"></i> {t('footer.workingHours')} , 09:00 – 20:00</p>
+
                 </div>
 
                 <h2 className="social-title">{t('footer.socialTitle')}</h2>
